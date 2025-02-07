f581c955fd2e7572f4242102c28f39cd5ab636b0
mbedekar <mandar.bedekar2007@gmail.com>
2025-02-06 16:18:17 -0800

    first commit

---
+++ eclipse-workspace/my-assignment/src/test/java/com/assignment/my_assignment/AppTest.java
@@ -0,0 +1,27 @@
+package com.assignment.my_assignment;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+    
+    public void shouldAnswerWithFalse() {
+        assertTrue(false);
+    }
+    
+    public void dummyTest() {
+        assertTrue(1==1);
+    }
+}
---
+++ eclipse-workspace/test_facebook/src/main/java/com/selenium/test_facebook/App.java
@@ -0,0 +1,10 @@
+package com.selenium.test_facebook;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
